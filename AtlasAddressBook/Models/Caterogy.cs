373dedfe5ca883dc17d4b7cf823087ff25b1f7da
Tyler0525 <96202897+Tyler0525@users.noreply.github.com>
2022-01-28 14:53:13 -0800

    Add project files.

---
+++ AtlasAddressBook/Models/Caterogy.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtlasAddressBook.Models
+{
+    public class Category
+    {
+        //Primary Key
+        public int Id { get; set; }
+
+        //Foreign Key
+        public string? UserId { get; set; }
+
+        [Required]
+        [Display (Name = "Category Name")]
+        public string? Name { get; set; }   
+
+        // Navingation Properties
+        public virtual AppUser? User { get; set; }
+
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+
+
+    }
+}
